7f0f82e821d1a3163b5093d7bfdaebb7cdf78f36
Anjali Patel <theanjali27@gmail.com>
2024-09-28 03:21:56 -0400

    database runs locally

--- HackTheHill/server/index.js
+++ HackTheHill/server/index.js
@@ -3,32 +3,8 @@ const  mongoose  = require('mongoose')
 
 const app= express()
 
-//mongoose.connect('mongodb+srv://lockedin:<db_password>@lockedin.s9ex3.mongodb.net/?retryWrites=true&w=majority&appName=lockedin')
+mongoose.connect('mongodb://localhost:27017/lockedin')
 
-
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://lockedin:<lockedin123>@lockedin.s9ex3.mongodb.net/?retryWrites=true&w=majority&appName=lockedin";
-
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true, //hii
-    deprecationErrors: true,
-  }
-});
-
-async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
-}
-run().catch(console.dir);
+app.listen(3001, () =>{
+  console.log("it works finally! ")
+})
