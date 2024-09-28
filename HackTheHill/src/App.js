7589d58b99a7c1bcf8ad6ef32caa86f7607bc6ab
Kimili2003 <147690293+Kimili2003@users.noreply.github.com>
2024-09-28 00:26:14 -0400

    Add files via upload
    "the Navbar and some file structure"

---
+++ HackTheHill/src/App.js
@@ -0,0 +1,17 @@
+// Import necessary modules from React
+import React from 'react';
+
+// Import the Navbar component
+import Navbar from './Components/Navbar/Navbar.js';
+
+function App() {
+    return (
+        <div className="App">
+            {/* Render the Navbar */}
+            <Navbar />
+            <h1>Welcome to HackTheHill</h1>
+        </div>
+    );
+}
+
+export default App;
---
+++ HackTheHill/src/Components/Navbar/Navbar.js
@@ -0,0 +1,18 @@
+// Import necessary modules from React
+import React from 'react';
+import './Navbar.css'; // Import CSS for Navbar styling
+
+// Define the Navbar component
+const Navbar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/ChatBox">ChatBox</a></li>
+                {/*<li><a href="/Login"></a></li>*/}
+            </ul>
+        </nav>
+    );
+};
+
+export default Navbar;
